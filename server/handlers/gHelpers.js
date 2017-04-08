e41f8cd6dc9e22fbdf0c98a2f941759b03837a37
justinjyoo@gmail.com <justinyoo@Justins-MacBook-Pro.local>
2017-04-08 14:35:06 -0700

    (bug) Lint gHelpers file

--- server/handlers/gHelpers.js
+++ server/handlers/gHelpers.js
@@ -1,32 +1,30 @@
-const axios = require('axios')
+const axios = require('axios');
 
-const geocode = location => {
-
+const geocode = (location) => {
   // uncomment the next line to receive an example request
   // let location = 'Beaverdam Creek Swamp'
-
   const config = {
     method: 'get',
     url: 'http://maps.googleapis.com/maps/api/geocode/json',
     headers: {
-      key: process.env.G_MAPS_GEOCODING_API_KEY,
+      key: process.env.G_MAPS_GEOCODING_API_KEY
     },
     params: {
-      address: location,
+      address: location
     }
+  };
+
+  return axios(config).then((response) => {
+    return response.data.results[0].geometry.location;
   }
-
-  return axios(config).then(response => {
-    return response.data.results[0].geometry.location
-  }).catch(error => {
-    console.log(error)
-  })
-
-}
+  ).catch((error) => {
+    throw error;
+  });
+};
 
 
 module.exports = {
   geocode: geocode
-}
+};
 
 // export default geocode
--- server/handlers/landmarksHelper.js
+++ server/handlers/landmarksHelper.js
@@ -1,28 +1,26 @@
 const XLSX = require('xlsx')
 
-// converts an xlsx file and saves it in an array
+// converts an xlsx file and saves it in an array.
 let nationalMonuments = []
 
-// uncomment lines 9, 19, 27 to check total number of national monuments
-// const numberOfMonuments = [];
+// uncomment to check total gnumber of national monuments.
 let workbook = XLSX.readFile(__dirname + '/National_Natural_Landmark.xlsx');
 
 const queryNationalLandmark = () => {
   let listOfStates = workbook.Props.SheetNames.splice(1)
-  let counter = 0;
+  // count for the number of monuments as a check
+  // let counter = 0;
 
-  // goes through each state sheet and pushes the monument to the list
+  // goes through each state sheet and creates a monument list for that state.
   listOfStates.forEach(state => {
     let i = 2;
     while (workbook.Sheets[state]['B' + i]) {
       let monument = workbook.Sheets[state]['B' + i].v
       nationalMonuments.push(monument)
       i++;
+      // counter++;
     }
   })
-
-  // uncomment to check total number of national monuments
-  // numberOfMonuments.forEach(a => counter++);
 }
 
 module.exports = {
