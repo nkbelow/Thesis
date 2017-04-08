d7dca103b8604fd641d6f2537fdaac3fa4e04e18
justinjyoo@gmail.com <justinyoo@Justins-MacBook-Pro.local>
2017-04-08 13:56:54 -0700

    (bug) Re-factor landmarksHelper.js to simplify output

--- server/handlers/landmarksHelper.js
+++ server/handlers/landmarksHelper.js
@@ -3,7 +3,7 @@ const XLSX = require('xlsx')
 // converts an xlsx file and saves it in an array
 let nationalMonuments = []
 
-// uncomment to check total number of national monuments
+// uncomment lines 9, 19, 27 to check total number of national monuments
 // const numberOfMonuments = [];
 let workbook = XLSX.readFile(__dirname + '/National_Natural_Landmark.xlsx');
 
@@ -11,22 +11,14 @@ const queryNationalLandmark = () => {
   let listOfStates = workbook.Props.SheetNames.splice(1)
   let counter = 0;
 
-  // goes through each state sheet and creates a monument list for that state
-  // example: {alabama: [array of monuments]}
+  // goes through each state sheet and pushes the monument to the list
   listOfStates.forEach(state => {
-    let monumentsListForState = [state]
     let i = 2;
     while (workbook.Sheets[state]['B' + i]) {
-<<<<<<< HEAD
-      numberOfMonuments.push(workbook.Sheets[state]['B' + i].v)
-=======
-      // numberOfMonuments.push(workbook.Sheets[state]['B' + i].v)
->>>>>>> Refine features
       let monument = workbook.Sheets[state]['B' + i].v
-      monumentsListForState.push(monument)
+      nationalMonuments.push(monument)
       i++;
     }
-    nationalMonuments.push(monumentsListForState)
   })
 
   // uncomment to check total number of national monuments
