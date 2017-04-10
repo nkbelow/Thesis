a9caa390759b5bbb2dd3be05b248da9ab8369c81
justinjyoo@gmail.com <justinyoo@Justins-MacBook-Pro.local>
2017-04-10 16:44:57 -0700

    Implement Google Places request handler

--- src/server/handlers/gHelpers.js
+++ src/server/handlers/gHelpers.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 
+
+// location is a {lat: lat, lng: lng} object
 const geocode = (location) => {
   // uncomment the next line to receive an example request
   // let location = 'Beaverdam Creek Swamp'
@@ -22,6 +24,28 @@ const geocode = (location) => {
 };
 
 
-module.exports.geocode = geocode;
+// example input
+// places({latitude: 44.5853962, longitude: -111.0759105}, 5000, 'lodging')
+  // radius and type are optional
+const places = (location, radius, type) => {
+  const config = {
+    method: 'get',
+    url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+    params: {
+      key: process.env.G_PLACES_API_KEY,
+      location: '' + location.latitude + ',' + location.longitude,
+      radius: radius || '50000',
+      type: type || 'lodging'
+    }
+  };
+  return axios(config).then((response) => {
+    console.log(response.data.results);
+  })
+}
+
+module.exports = {
+  geocode: geocode,
+  places: places
+};
 
 // export default geocode
--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const dotenv = require('dotenv').config();
 
+const gHelpers = require('./handlers/gHelpers.js')
+
 const app = express();
 const port = 3000;
 
