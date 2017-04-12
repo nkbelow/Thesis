9b57520ca0dcc6ef599dee9f3fade41ebd0ecf51
justinjyoo@gmail.com <justinyoo@Justins-MacBook-Pro.local>
2017-04-11 18:55:20 -0700

    (feat) Implement trails database storage script

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  console.log(trail.storeTrails());
+  trail.storeTrails();
   res.status(200).send('hello world!!!');
 });
 
