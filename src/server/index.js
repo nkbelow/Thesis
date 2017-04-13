58e3651f5b7d715f7a84a54911ce74c0786338e9
justinjyoo <justinjyoo@gmail.com>
2017-04-13 13:31:13 -0700

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
-// const landmarks = require('./handlers/landmarksHelper');
-// const campgrounds = require('./handlers/campgroundsHelper');
-// const geocode = require('./handlers/gHelpers');
-// const util = require('./utilities');
-// const fs = require('fs');
+const landmarks = require('./handlers/landmarksHelper');
+const campgrounds = require('./handlers/campgroundsHelper');
+const geocode = require('./handlers/gHelpers');
+const util = require('./utilities');
+const fs = require('fs');
 const path = require('path');
-// const dotenv = require('dotenv').config();
-// const trail = require('../db/scripts/storeTrails.js')
-// const bodyParser = require('body-parser');
-// const path = require('path');
-// const cg = require('../db/scripts/storeCampgrounds')
+const dotenv = require('dotenv').config();
+const trail = require('../db/scripts/storeTrails.js')
+const bodyParser = require('body-parser');
+const path = require('path');
+const cg = require('../db/scripts/storeCampgrounds')
 const app = express();
 const port = process.env.PORT || 3000;
-// const db = require('../db/index.js');
-// const data = require('../../data/ourNationalParks.js')
+const db = require('../db/index.js');
+const data = require('../../data/ourNationalParks.js')
 
 
 
