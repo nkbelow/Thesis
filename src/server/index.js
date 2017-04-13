8952e72c8948154bcf256895ab60429e4f915669
justinjyoo <justinjyoo@gmail.com>
2017-04-13 13:39:31 -0700

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -16,43 +16,43 @@ app.get('/', (req, res) => {
   res.status(200).send('hello world!!!');
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/parks', (req, res) => {
-	// db.db.query('SELECT * from parks')
-// 	.then((result) => {
-// 		res.status(201).send(result)
-// 	})
-// 	.catch((err) => {
-// 		res.status(404).send('There was an error retrieving all the parks data');
-// 	})
-	res.send(data.ourNationalParks);
+// app.get('/api/parks', (req, res) => {
+// 	// db.db.query('SELECT * from parks')
+// // 	.then((result) => {
+// // 		res.status(201).send(result)
+// // 	})
+// // 	.catch((err) => {
+// // 		res.status(404).send('There was an error retrieving all the parks data');
+// // 	})
+// 	res.send(data.ourNationalParks);
 
-});
+// });
 
-app.get('/api/park', (req, res) => {
-	// db.db.query('SELECT * from parks WHERE parkcode = $1', [req.query.parkcode])
-	// .then((result) => {
-	// 	res.status(201).send(result)
-	// })
-	// .catch((err) => {
-	// 	res.status(404).send('There was an error retrieving park data');
-	// })
-	var parks = data.ourNationalParks;
-	for(var i = 0; i < parks.length; i++){
-		if(parks[i].parkCode === req.query.parkcode){
-			res.status(200).send(parks[i]);
-		}
-	}
-})
+// app.get('/api/park', (req, res) => {
+// 	// db.db.query('SELECT * from parks WHERE parkcode = $1', [req.query.parkcode])
+// 	// .then((result) => {
+// 	// 	res.status(201).send(result)
+// 	// })
+// 	// .catch((err) => {
+// 	// 	res.status(404).send('There was an error retrieving park data');
+// 	// })
+// 	var parks = data.ourNationalParks;
+// 	for(var i = 0; i < parks.length; i++){
+// 		if(parks[i].parkCode === req.query.parkcode){
+// 			res.status(200).send(parks[i]);
+// 		}
+// 	}
+// })
 
-app.get('*', (req, res) => {
-	res.redirect('/');
-})
+// app.get('*', (req, res) => {
+// 	res.redirect('/');
+// })
 
-app.listen(port, () => {
-  console.log('App is listening to port ' + port);
-});
+// app.listen(port, () => {
+//   console.log('App is listening to port ' + port);
+// });
 
-module.exports = app;
+// module.exports = app;
