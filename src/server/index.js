b3ba1f722020464935c97abe27900f64e953ef9c
justinjyoo <justinjyoo@gmail.com>
2017-04-13 13:28:08 -0700

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3000;
 
 
 
-// app.use('/', express.static(path.join(__dirname, '../client/public')));
+app.use('/', express.static(path.join(__dirname, '../client/public')));
 
 app.get('/', (req, res) => {
   res.status(200).send('hello world!!!');
