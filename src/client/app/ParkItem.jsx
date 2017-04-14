981b65cc8b8b95f9c8b97a69233150729a6dd70b
justinjyoo@gmail.com <justinyoo@Justins-MacBook-Pro.local>
2017-04-13 20:52:04 -0700

    (bug) Remove hard-coded key for cloudinary helper

--- src/client/app/ParkItem.jsx
+++ src/client/app/ParkItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import Cloudinary from '../../server/handlers/cloudinary.js'
 
 class ParkItem extends React.Component {
   constructor(props) {
