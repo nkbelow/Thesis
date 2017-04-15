6942896e607d9be743453240a1f43cfb3190cf2b
NicholasBelow <nkbelow@gmail.com>
2017-04-14 21:03:59 -0700

    feat comment out marker and refactor query for single page

--- src/db/models/getIndividualParksInfo.js
+++ src/db/models/getIndividualParksInfo.js
@@ -1,7 +1,10 @@
 const db = require('../index.js').db;
 
 module.exports = (parkCode) => {
-  let query1 = 'SELECT activity FROM activities inner join activities_parks on (activities_parks.activity_id = activities.id AND activities_parks.park_id = (select id from parks where parkcode = $1))'; 
+  let query1 = 'SELECT activity FROM activities \
+   inner join activities_parks on \
+   (activities_parks.activity_id = activities.id AND activities_parks.park_id \
+   = (select id from parks where parkcode = $1))'; 
   let query2 = 'SELECT * FROM parks where parkcode =$1';
   // let query3 = 'SELECT * FROM campground where park_id = $1';
   // let query4 = 'SELECT * FROM trails where park_id = $1';
