b6d722d651a5a89a88caf1fab5eb561e4d7355c2
Tyler Walker <tyjohnwalker@gmail.com>
2017-04-15 15:27:21 -0700

    remove console logs

--- src/client/app/ParkView.jsx
+++ src/client/app/ParkView.jsx
@@ -15,16 +15,13 @@ class ParkView extends React.Component {
         }
       })
       .then(res => {
-        console.log(res)
         this.setState({ park: res.data[1][0], activities: res.data[0]});
-        console.log('single page view data: ', res.data);
         })
       .catch(err => console.log(err))
   }
 
 
   render() {
-    console.log(this.state.park)
     return(
     	<div>
     		{ this.state.park !== null && <ParkDetail park={this.state.park} activities={this.state.activities} /> }
--- src/server/index.js
+++ src/server/index.js
@@ -35,7 +35,6 @@ app.get('/api/parks', (req, res) => {
 app.get('/api/park', (req, res) => {
 	individualParkData(req.query.parkcode)
 	.then((data) => {
-		console.log(data);
 		let park = data;
 		res.status(200).send(data);
 	});
