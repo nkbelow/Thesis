60ba5d13bcbb6c3aae250e78bcb03e8b5f9cc08f
Neil Ricci <iccir919@gmail.com>
2017-04-15 16:47:01 -0700

    /bug heroku database

--- src/db/index.js
+++ src/db/index.js
@@ -45,22 +45,22 @@ const database = 'thesis';
 // module.exports.db = pgp(connection)
 // pgp.end()
 
-
+	console.log('this is is env', process.env.DATABASE_URL);
 // NEW CODE STARTS HERE
-if (process.env.HEROKU) {
-	console.log(process.env.DATABASE_URL);
+if (process.env.DATABASE_URL) {
+
   pgp.pg.defaults.ssl = true;
 
   let db = pgp(process.env.DATABASE_URL);
 
   module.exports.db = db;
 } 
-// else {
-//   let db = pgp({
-//     database: database
-//   });
+else {
+  let db = pgp({
+    database: database
+  });
 
-//   module.exports.db = db;
-// }
+  module.exports.db = db;
+}
 
 
