dbe9eb81070587cf1f97230d198f44455bf02fd8
justinjyoo@gmail.com <justinyoo@Justins-MacBook-Pro.local>
2017-04-17 12:31:56 -0700

    (fix) Refactor the database index.js to handle heroku deployment and local database creation

--- src/db/index.js
+++ src/db/index.js
@@ -1,65 +1,47 @@
-var promise = require('bluebird');
-var createTables = require('./tablesConfig.js');
-var databaseName = 'thesis';
-var options = {
+const promise = require('bluebird');
+const createTables = require('./tablesConfig.js');
+const databaseName = 'thesis';
+const options = {
 	promiseLib: promise
 };
-// let db;
-var pgp = require('pg-promise')();
-const database = 'thesis';
-
-// var database = pgp({})
-
-// comment out when you run test
-// database.query('SELECT count(*) FROM pg_catalog.pg_database WHERE DATNAME = \'thesis\'').then(function (response) {
-//   const databaseExists = parseInt(response[0].count)
-//   if(!databaseExists) {
-//     console.log('create database')
-// 	 return database.query('CREATE DATABASE thesis'); 
-//   } else {
-//     return databaseExists;
-//   }
-// }).then(function () {
-// 	pgp.end();
-// 	db = pgp({database: databaseName})
-// 	return db;
-// }).then(function(db) {
-//   createTables(db); 
-//   console.log('tables')
-//   pgp.end();
-//   return db;
-// })
-
-
-
-// var connection = {
-// 	  host: 'localhost',
-//     port: 5432,
-//     database: databaseName,
-//     user: '',
-//     password: ''
-// }
-// re-establish a connection to export as a module
-// will come back to re-factor to connecting pgp once
-
-// module.exports.db = pgp(connection)
-// pgp.end()
-
+const pgp = require('pg-promise')(options);
 
 if (process.env.DATABASE_URL) {
-
   pgp.pg.defaults.ssl = true;
 
   let db = pgp(process.env.DATABASE_URL);
-
   module.exports.db = db;
 } 
 else {
-  let db = pgp({
-    database: database
-  });
+  let database = pgp({})
 
-  module.exports.db = db;
+  database.query('SELECT count(*) FROM pg_catalog.pg_database WHERE DATNAME = \'thesis\'').then(function (response) {
+    const databaseExists = parseInt(response[0].count)
+    if(!databaseExists) {
+     return database.query('CREATE DATABASE thesis'); 
+    } else {
+      return databaseExists;
+    }
+  }).then(function () {
+    pgp.end();
+    db = pgp({database: databaseName})
+    return db;
+  }).then(function(db) {
+    createTables(db); 
+    pgp.end();
+    return db;
+  })
+
+  const connection = {
+      host: 'localhost',
+      port: 5432,
+      database: databaseName,
+      user: '',
+      password: ''
+  }
+
+  module.exports.db = pgp(connection);
+  pgp.end();
 }
 
 
