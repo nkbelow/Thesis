aefddbccf96745ba6849e7c50e11950e78e984ac
justinjyoo@gmail.com <justinyoo@Justins-MacBook-Pro.local>
2017-04-18 10:35:59 -0700

    Uncomment the trailshelper function

--- src/server/handlers/trailsHelper.js
+++ src/server/handlers/trailsHelper.js
@@ -1,29 +1,29 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// const trails = (location) => {
-//   const config = {
-//     method: 'get',
-//     url: 'https://trailapi-trailapi.p.mashape.com/',
-//     headers: {
-//       'X-Mashape-Key': process.env.X_Mashape_Key,
-//     },
-//     params: {
-//       lat: location.lat,
-//       lon: location.lng,
-//       limit: '100',
-//       // 'q[activities_activity_type_name_eq]': 'hiking',
-//       // 'q[country_cont]': 'United States',
-//       radius: '35'
-//     }
-//   };
-//   return axios(config).then((response) => {
-//     return response.data.places;
-//   }).catch((error) => {
-//     throw error;
-//   });
-// };
+const trails = (location) => {
+  const config = {
+    method: 'get',
+    url: 'https://trailapi-trailapi.p.mashape.com/',
+    headers: {
+      'X-Mashape-Key': process.env.X_Mashape_Key,
+    },
+    params: {
+      lat: location.lat,
+      lon: location.lng,
+      limit: '100',
+      // 'q[activities_activity_type_name_eq]': 'hiking',
+      // 'q[country_cont]': 'United States',
+      radius: '35'
+    }
+  };
+  return axios(config).then((response) => {
+    return response.data.places;
+  }).catch((error) => {
+    throw error;
+  });
+};
 
-// module.exports = {
-//   trails: trails
-// };
+module.exports = {
+  trails: trails
+};
 
