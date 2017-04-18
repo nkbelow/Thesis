ce6faf5077263fc552b890a66d6ddc39a1b329b9
NicholasBelow <nkbelow@gmail.com>
2017-04-18 10:36:21 -0700

    feat Add sidebar to landing page to hold filters

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import Filter from './Filter.jsx';
+import FilterRow from './FilterRow.jsx'
 import ParkList from './ParkList.jsx';
 import ParkView from './ParkView.jsx';
 import SearchBar from './SearchBar.jsx';
 import MapView from './MapBox.jsx';
 import axios from 'axios';
+import SidebarFilters from './sidebarFilters.jsx';
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header, List, Accordion } from 'semantic-ui-react';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       parks: null,
-      filteredParks: null
+      filteredParks: null,
+      visible: false
+      
+
       };
 
       // this.setActivePark = this.setActivePark.bind(this)
@@ -32,11 +37,11 @@ class App extends React.Component {
     return new Promise((resolve, reject) => {
       if (selectedState) {
         this.filteredActivitiesArray.push(filter);
-        resolve(this.setState({filteredActivities: this.filteredActivitiesArray}))
+        resolve(this.filteredActivitiesArray)
       } else if (!selectedState) {
         const index = this.filteredActivitiesArray.indexOf(filter);
         this.filteredActivitiesArray.splice(index, 1);
-        resolve(this.setState({filteredActivities: this.filteredActivitiesArray}))
+        resolve(this.filteredActivitiesArray)
       }
     }).then(() => {
       if (this.filteredActivitiesArray.length === 0) {
@@ -44,7 +49,7 @@ class App extends React.Component {
       } else {
         axios.get('/filterparks', {
           params: {
-            filteredActivities: this.state.filteredActivities
+            filteredActivities: this.filteredActivitiesArray
           }
         }).then((response) => {
           this.setState({filteredParks: response.data})
@@ -54,18 +59,30 @@ class App extends React.Component {
       }
     })
   }
+  toggleVisibility() {
+    this.setState({ visible: !this.state.visible })
+  }
 
 
   render () {
     return (
     	<div>
-
-    		<h1>Nimble Newts Project </h1>
+         <Button onClick={this.toggleVisibility.bind(this)}>Toggle Visibility</Button>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} animation='push' width='thin' visible={!this.state.visible} icon='labeled' vertical inverted>
+          <SidebarFilters handleFilterActivities={this.handleFilterActivities.bind(this)} visible={!this.state.visible} />
+          </Sidebar>
+          <Sidebar.Pusher>
+          <Segment basic>
+          <h1>Nimble Newts Project </h1>
         {this.state.parks !== null && <SearchBar parks={this.state.parks}/>}
-        <Filter handleActivitiesClick={this.handleFilterActivities.bind(this)} />
         <MapView parks={this.state.filteredParks}/>
-    		{this.state.parks !== null && <ParkList parks={this.state.filteredParks}/>}
-    	</div>
+        {this.state.parks !== null && <ParkList parks={this.state.filteredParks}/>}
+          </Segment> 
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+        </div>
+
     );
   }
 }
