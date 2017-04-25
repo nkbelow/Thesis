00b01048084c9f9ece2f5123fa1d321a27d94762
NicholasBelow <nkbelow@gmail.com>
2017-04-24 22:38:01 -0700

    feat add weather handler

--- src/server/handlers/weatherHandlers/historicalWeatherData.js
+++ src/server/handlers/weatherHandlers/historicalWeatherData.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
 
-let getClimate = (req, res) => {
+let getHistoricalData = (req, res) => {
   //longitude will need to come from req
   //latitude will need to come from req
   //dates will need to come from req
+  console.log(req.query, 'this is the query data');
   let config = {
     url: 'http://api.wunderground.com/api/' + process.env.WEATHER_UNDERGROUND_API_KEY +
      '/planner_07010731/q/37.776289,-122.395234.json',
@@ -19,4 +20,4 @@ let getClimate = (req, res) => {
 }
 
 
-module.exports = {getClimate: getClimate};
+module.exports = {getHistoricalData};
