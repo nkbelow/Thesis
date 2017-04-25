edb4b7049ba4d69877219dfc757ff533e7d0be25
NicholasBelow <nkbelow@gmail.com>
2017-04-25 10:03:41 -0700

    feat add functionality for historical weather data

--- src/server/handlers/weatherHandlers/historicalWeatherData.js
+++ src/server/handlers/weatherHandlers/historicalWeatherData.js
@@ -1,17 +1,13 @@
 const axios = require('axios');
 
 let getHistoricalData = (req, res) => {
-  //longitude will need to come from req
-  //latitude will need to come from req
-  //dates will need to come from req
-  console.log(req.query, 'this is the query data');
   let config = {
     url: 'http://api.wunderground.com/api/' + process.env.WEATHER_UNDERGROUND_API_KEY +
      '/planner_' + req.query.month + '/q/' + req.query.latitude + ',' + req.query.longitude + '.json',
     method: 'get',
   }
   return axios(config).then((result) => {
-    console.log()
+    console.log(result)
     res.status(200).send(result['data']);
   })
   .catch((err) => {
